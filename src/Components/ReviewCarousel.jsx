2967135a5477f54b7d7a6ec6971bcf4eb99f1f81
Global Visa Internationals <globalvisainternationals25@gmail.com>
2025-11-06 12:46:53 +0530

    Update ReviewCarousel.jsx

--- src/Components/ReviewCarousel.jsx
+++ src/Components/ReviewCarousel.jsx
@@ -1,11 +1,8 @@
 import reviews from "@/data/reviews.json";
 import ReviewCarouselClient from "@/Components/ReviewCard";
-
 export default function ReviewCarousel() {
     return (
         <>
-
-
             {/* ✅ Interactive carousel for UX */}
             <ReviewCarouselClient reviews={reviews} />
         </>
