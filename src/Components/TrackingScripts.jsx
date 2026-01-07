f75d0f8a6a93da410439e48c2850f4039a1366e1
Global Visa Internationals <globalvisainternationals25@gmail.com>
2026-01-07 16:31:41 +0530

    pixel update

--- src/Components/TrackingScripts.jsx
+++ src/Components/TrackingScripts.jsx
@@ -2,44 +2,63 @@
 'use client' // This must be a client component to handle window events
 
 import Script from 'next/script'
+const META_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+const CLARITY_ID = process.env.NEXT_PUBLIC_CLARITY_ID;
 
 export default function TrackingScripts() {
     return (
         <>
-            {/* --- META PIXEL (Facebook) --- */}
-            <Script
-                id="fb-pixel"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', 'YOUR_META_PIXEL_ID'); // <--- REPLACE THIS
-            fbq('track', 'PageView');
-          `,
-                }}
-            />
+            {/* --- META PIXEL --- */}
+            {META_ID && (
+                <>
+                    <Script
+                        id="fb-pixel"
+                        strategy="afterInteractive"
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${META_ID}');
+                fbq('track', 'PageView');
+              `,
+                        }}
+                    />
+                    <noscript>
+                        <img
+                            height="1"
+                            width="1"
+                            style={{ display: 'none' }}
+                            src={`https://www.facebook.com/tr?id=${META_ID}&ev=PageView&noscript=1`}
+                            alt="meta pixel"
+                        />
+                    </noscript>
+                </>
+            )}
 
-            {/* --- MICROSOFT CLARITY (Heatmaps) --- */}
-            <Script
-                id="microsoft-clarity"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "YOUR_CLARITY_ID"); // <--- REPLACE THIS
-          `,
-                }}
-            />
+
+
+            {/* --- MICROSOFT CLARITY --- */}
+            {CLARITY_ID && (
+                <Script
+                    id="microsoft-clarity"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${CLARITY_ID}");
+            `,
+                    }}
+                />
+            )}
 
             {/* --- DATA ENRICHMENT (Example: Clearbit) --- */}
             {/* Most enrichment tools work the same way. Paste their script here. */}
